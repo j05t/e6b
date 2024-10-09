3c3825d8a0b111072f19ebc865afd7f225f65364
j05t <josef.steppan@gmail.com>
2024-10-09 14:57:08 +0200

    lock to portrait screen orientation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jstappdev.e6bflightcomputer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1"
+        versionCode = 12
+        versionName = "1.2"
     }
 
     buildTypes {
