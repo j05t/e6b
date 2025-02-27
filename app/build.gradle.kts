82889314162b1e980594e6df8bc1533c0758e5bd
j05t <josef.steppan@gmail.com>
2025-02-27 10:04:04 +0100

    Fixed button layout for Google Pixel 9 Pro XL.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jstappdev.e6bflightcomputer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.3"
+        versionCode = 14
+        versionName = "1.4"
     }
 
     buildTypes {
